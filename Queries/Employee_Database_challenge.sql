aa839c4f5587d42086be6b2bc3528008125442c0
Thomas Allen <trallen09@yahoo.com>
2021-04-25 18:51:39 -0500

    final

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,70 @@
+--Mod 7 Challenge
+SELECT *
+FROM employees;
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+SELECT *
+FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+SELECT COUNT(emp_no)
+FROM unique_titles;
+
+
+
+--15.
+SELECT *
+FROM unique_titles;
+
+SELECT COUNT(ut.emp_no) AS empcount, ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title
+ORDER BY empcount DESC; 
+
+SELECT *
+FROM retiring_titles;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+INNER JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+SELECT *
+FROM dept_emp
+SELECT *
+FROM titles
+SELECT *
+FROM employees
